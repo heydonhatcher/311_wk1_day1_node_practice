76de93be720d9e3ba4f1c3f60aca5f7634b1a4b7
heydonhatcher <heyhatch@gmail.com>
2019-11-09 16:22:32 -0600

    ready to grade

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -10,7 +10,23 @@ const sum = arr => {
   return arr.reduce((a, b) => a + b, 0);
 };
 
-const comboSum = (arr, sum) => {};
+const comboSum = (arr, sum) => {
+  var match = false;
+  arr.forEach(num => {
+    for (i = 0; i < arr.length; i++) {
+      if (match === true) {
+        break;
+      }
+      if (arr.indexOf(num) !== i) {
+        if (num + arr[i] === sum) {
+          match = true;
+          return match;
+        }
+      }
+    }
+  });
+  return match;
+};
 
 module.exports = {
   isEven,
